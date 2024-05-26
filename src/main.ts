0c9f62b84b586a360d8eced8945fbeb2ab458877
Cédric Dinont <cedric@ecole2600.com>
2024-05-26 16:59:12 +0200

    add websocket

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,18 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
+
+const socket = new WebSocket('ws://localhost:4545');
+
+// Connection opened
+socket.addEventListener('open', (event) => {
+  socket.send('Hello Server!');
+});
+
+// Listen for messages
+socket.addEventListener('message', (event) => {
+  console.log('Message from server ', event.data);
+});
